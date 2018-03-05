3a9d422233fe32accdca0df15d155a2579346de8
aoyagikouhei <aoyagi.kouhei@gmail.com>
2018-03-06 08:44:59 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,213 @@
+extern crate base64;
+extern crate reqwest;
+extern crate serde_json;
+
+pub mod oauth1;
+pub mod oauth2;
+
+#[derive(Debug)]
+pub enum TwapiError {
+    Connection(reqwest::Error),
+    JSON(serde_json::Error),
+    NotExists,
+}
+
+impl From<reqwest::Error> for TwapiError {
+    fn from(err: reqwest::Error) -> TwapiError {
+        TwapiError::Connection(err)
+    }
+}
+
+impl From<serde_json::Error> for TwapiError {
+    fn from(err: serde_json::Error) -> TwapiError {
+        TwapiError::JSON(err)
+    }
+}
+
+pub trait Twapi {
+    fn get(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError>;
+    fn post(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError>;
+    fn delete(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError>;
+    fn json(&self, uri: &str, value: &serde_json::Value) -> Result<reqwest::Response, TwapiError>;
+
+    fn get_search_tweets(
+        &self,
+        params: &Vec<(&str, &str)>
+    ) -> Result<serde_json::Value, TwapiError> {
+        let res = self.get(
+            "https://api.twitter.com/1.1/search/tweets.json",
+            params
+        )?.json()?;
+        Ok(res)
+    }
+
+    fn post_statuses_update(
+        &self,
+        params: &Vec<(&str, &str)>
+    ) -> Result<serde_json::Value, TwapiError> {
+        let res = self.post(
+            "https://api.twitter.com/1.1/statuses/update.json",
+            params
+        )?.json()?;
+        Ok(res)
+    }
+
+    fn post_direct_messages_events_new(
+        &self,
+        value: &serde_json::Value
+    ) -> Result<serde_json::Value, TwapiError> {
+        let res = self.json(
+            "https://api.twitter.com/1.1/direct_messages/events/new.json",
+            value
+        )?.json()?;
+        Ok(res)
+    }
+
+    fn get_account_activity_subscription(
+        &self,
+        env_name: &str
+    ) -> Result<serde_json::Value, TwapiError> {
+        let mut res = self.get(
+            format!("https://api.twitter.com/1.1/account_activity/all/{}/subscriptions.json", env_name).as_str(),
+            &vec![]
+        )?;
+        if res.status().as_u16() == 204 {
+            Ok(serde_json::from_str("{}")?)
+        } else {
+            Ok(res.json()?)
+        }
+    }
+
+    fn get_direct_messages_welcome_messages_list(
+        &self
+    ) -> Result<serde_json::Value, TwapiError> {
+        let res = self.get(
+            "https://api.twitter.com/1.1/direct_messages/welcome_messages/list.json",
+            &vec![]
+        )?.json()?;
+        Ok(res)
+    }
+
+    fn get_direct_messages_welcome_messages_show(
+        &self,
+        id: &str
+    ) -> Result<serde_json::Value, TwapiError> {
+        let res = self.get(
+            "https://api.twitter.com/1.1/direct_messages/welcome_messages/show.json",
+            &vec![("id", id)]
+        )?.json()?;
+        Ok(res)
+    }
+
+    fn post_direct_messages_welcome_messages_new(
+        &self,
+        value: &serde_json::Value
+    ) -> Result<serde_json::Value, TwapiError> {
+        let res = self.json(
+            "https://api.twitter.com/1.1/direct_messages/welcome_messages/new.json",
+            value
+        )?.json()?;
+        Ok(res)
+    }
+
+    fn delete_direct_messages_welcome_messages_destroy(
+        &self,
+        id: &str
+    ) -> Result<serde_json::Value, TwapiError> {
+        let mut res = self.delete(
+            "https://api.twitter.com/1.1/direct_messages/welcome_messages/destroy.json",
+            &vec![("id", id)]
+        )?;
+        if res.status().as_u16() == 204 {
+            Ok(serde_json::from_str("{}")?)
+        } else {
+            Ok(res.json()?)
+        }
+    }
+}
+
+pub struct UserAuth {
+    token: self::oauth1::Token
+}
+
+impl UserAuth {
+    pub fn new (
+        consumer_key: &str, 
+        consumer_secret: &str, 
+        access_token: &str, 
+        access_token_secret: &str
+    ) -> UserAuth {
+        UserAuth {
+            token: self::oauth1::Token::new(
+                consumer_key,
+                consumer_secret,
+                access_token,
+                access_token_secret
+            )
+        }
+    }
+}
+
+impl Twapi for UserAuth {
+    fn get(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError> {
+        Ok(self.token.get(uri, options)?)
+    }
+
+    fn post(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError> {
+        Ok(self.token.post(uri, options)?)
+    }
+
+    fn delete(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError> {
+        Ok(self.token.delete(uri, options)?)
+    }
+
+    fn json(&self, uri: &str, value: &serde_json::Value) -> Result<reqwest::Response, TwapiError> {
+        Ok(self.token.json(uri, value)?)
+    }
+}
+
+pub struct ApplicationAuth {
+    baerer_token: String
+}
+
+impl ApplicationAuth {
+    pub fn new (
+        baerer_token: &str
+    ) -> ApplicationAuth {
+        ApplicationAuth {
+            baerer_token: String::from(baerer_token)
+        }
+    }
+
+    pub fn new_with_consumer (
+        consumer_key: &str, 
+        consumer_secret: &str
+    ) -> ApplicationAuth {
+        let baerer_token = self::oauth2::get_bearer_token(
+            consumer_key, 
+            consumer_secret, 
+            "https://api.twitter.com/oauth2/token", 
+            &vec![("grant_type", "client_credentials")]);
+        ApplicationAuth {
+            baerer_token: baerer_token.unwrap()
+        }
+    }
+}
+
+impl Twapi for ApplicationAuth {
+    fn get(&self, uri: &str, options: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError> {
+        Ok(self::oauth2::get(&self.baerer_token, uri, options)?)
+    }
+
+    fn delete(&self, _: &str, _: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError> {
+        Err(TwapiError::NotExists)
+    }
+
+    fn post(&self, _: &str, _: &Vec<(&str, &str)>) -> Result<reqwest::Response, TwapiError> {
+        Err(TwapiError::NotExists)
+    }
+
+    fn json(&self, _: &str, _: &serde_json::Value) -> Result<reqwest::Response, TwapiError> {
+        Err(TwapiError::NotExists)
+    }
+}
---
+++ src/oauth1.rs
@@ -0,0 +1,148 @@
+extern crate base64;
+extern crate chrono;
+extern crate crypto;
+extern crate rand;
+extern crate url;
+extern crate reqwest;
+extern crate serde_json;
+
+use self::rand::{Rng, thread_rng};
+use self::chrono::prelude::*;
+use self::crypto::sha1::Sha1;
+use self::crypto::hmac::Hmac;
+use self::crypto::mac::Mac;
+use self::reqwest::header::{Headers, Authorization};
+
+fn nonce() -> String {
+    thread_rng().gen_ascii_chars().take(32).collect::<String>()
+}
+
+fn timestamp() -> String {
+    format!("{}", Utc::now().timestamp())
+}
+
+fn encode(s: &str) -> String {
+    url::form_urlencoded::byte_serialize(s.as_bytes()).collect::<String>()
+}
+
+fn sign(base: &str, key: &str) -> String {
+    let mut hmac = Hmac::new(Sha1::new(), key.as_bytes());
+    hmac.input(base.as_bytes());    
+    base64::encode(hmac.result().code())
+}
+
+fn make_query(list: &Vec<(&str, String)>, separator: &str) -> String {
+    let mut result = String::from("");
+    for item in list {
+        if "" != result {
+            result.push_str(separator);
+        }
+        result.push_str(&format!("{}={}", item.0, item.1));
+    }
+    result
+}
+
+pub struct Token {
+    consumer_key: String,
+    consumer_secret: String,
+    access_token: String,
+    access_token_secret: String
+}
+
+impl Token {
+    pub fn new(
+        consumer_key: &str, 
+        consumer_secret: &str, 
+        access_token: &str, 
+        access_token_secret: &str
+    ) -> Token {
+        Token {
+            consumer_key: String::from(consumer_key),
+            consumer_secret: String::from(consumer_secret),
+            access_token: String::from(access_token),
+            access_token_secret: String::from(access_token_secret),
+        }
+    }
+
+    pub fn calc_oauth_header(&self, method: &str, uri: &str, options: &Vec<(&str, &str)>) -> String {
+        let param0: Vec<(&str, String)> = vec![
+            ("oauth_consumer_key", self.consumer_key.clone()),
+            ("oauth_nonce", nonce()),
+            ("oauth_signature_method", String::from("HMAC-SHA1")),
+            ("oauth_timestamp", timestamp()),
+            ("oauth_token", self.access_token.clone()),
+            ("oauth_version", String::from("1.0"))
+        ];
+        let mut param1 = param0.clone();
+        for option in options {
+            param1.push((option.0, encode(option.1)));
+        }
+        param1.sort();
+        let parameter = make_query(&param1, "&");
+        let base = format!("{}&{}&{}", method, encode(uri), encode(&parameter));
+        let key = format!("{}&{}", &self.consumer_secret, &self.access_token_secret);
+        let mut param2 = param0.clone();
+        param2.push(("oauth_signature", encode(&sign(&base, &key))));
+        make_query(&param2, ", ")
+    }
+
+    pub fn make_oauth_header(&self, method: &str, uri: &str, options: &Vec<(&str, &str)>) -> String {
+        format!("OAuth {}", self.calc_oauth_header(method, uri, options))
+    }
+
+    pub fn get(
+        &self, uri: &str, options: &Vec<(&str, &str)>
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let mut headers = Headers::new();
+        headers.set(Authorization(
+            self.make_oauth_header("GET", uri, &options)));
+        let client = reqwest::Client::new();
+        client
+            .get(uri)
+            .headers(headers)
+            .query(&options)
+            .send()
+    }
+
+    pub fn post(
+        &self, uri: &str, options: &Vec<(&str, &str)>
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let mut headers = Headers::new();
+        headers.set(Authorization(
+            self.make_oauth_header("POST", uri, &options)));
+        let client = reqwest::Client::new();
+        client
+            .post(uri)
+            .headers(headers)
+            .form(&options)
+            .send()
+    }
+
+    pub fn delete(
+        &self, uri: &str, options: &Vec<(&str, &str)>
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let mut headers = Headers::new();
+        headers.set(Authorization(
+            self.make_oauth_header("DELETE", uri, &options)));
+        let client = reqwest::Client::new();
+        client
+            .delete(uri)
+            .headers(headers)
+            .query(&options)
+            .send()
+    }
+
+    pub fn json(
+        &self, uri: &str, value: &serde_json::Value
+    ) -> Result<reqwest::Response, reqwest::Error> {
+        let mut headers = Headers::new();
+        headers.set(Authorization(
+            self.make_oauth_header("POST", uri, &vec![])));
+        let client = reqwest::Client::new();
+        client
+            .post(uri)
+            .headers(headers)
+            .json(&value)
+            .send()
+    }
+}
